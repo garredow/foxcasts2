d5701d3f3e26047b763793d4c7e9433916483e52
Garrett Downs <choorp@gmail.com>
2018-08-25 00:03:23 -0400

    Add utility function formatTIme

---
+++ src/utils/formatTime.js
@@ -0,0 +1,14 @@
+const formatTime = rawSeconds => {
+  const hours = Math.floor(rawSeconds / 60 / 60);
+  const minutes = Math.floor((rawSeconds / 60) % 60);
+  const seconds = Math.floor(rawSeconds % 60);
+
+  let time = '';
+  time += hours > 0 ? hours.toString() + ':' : '';
+  time += minutes > 0 ? minutes.toString().padStart(2, '0') + ':' : '0:';
+  time += seconds.toString().padStart(2, '0');
+
+  return time;
+};
+
+export default formatTime;
