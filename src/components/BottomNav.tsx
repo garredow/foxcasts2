ef8c9281305d323c7bb1bd8b605693c569c34325
Garrett Downs <choorp@gmail.com>
2018-12-09 12:45:55 -0500

    Add BottomNav filter drawer

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -11,6 +11,13 @@ import { compose } from 'recompose';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
 import { EpisodeExtended } from '../models';
 import PlayerButton from './PlayerButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Drawer from '@material-ui/core/Drawer';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import TimeIcon from '@material-ui/icons/AccessTime';
+import ProgressIcon from '@material-ui/icons/AvTimer';
 
 const styles: any = (theme: Theme) => {
   return {
@@ -19,10 +26,25 @@ const styles: any = (theme: Theme) => {
       bottom: 0,
       left: 0,
       right: 0,
+      zIndex: 9999,
       borderTop: `1px solid ${theme.palette.divider}`,
+    },
+    filterDrawerPaper: {
+      paddingBottom: '60px',
     },
   };
 };
+
+type Filter = {
+  id: string;
+  title: string;
+  icon: any;
+};
+
+const filters: Filter[] = [
+  { id: 'filter-recent', title: 'Most Recent', icon: <TimeIcon /> },
+  { id: 'filter-inProgress', title: 'In Progress', icon: <ProgressIcon /> },
+];
 
 type OwnProps = {
   episode?: EpisodeExtended;
@@ -32,27 +54,40 @@ type BottomNavProps = OwnProps & WithStyles & RouteComponentProps;
 
 type BottomNavState = {
   selectedTab: string;
+  filterDrawerOpen: boolean;
 };
 
 class BottomNav extends React.Component<BottomNavProps, BottomNavState> {
   state: BottomNavState = {
     selectedTab: 'subscriptions',
+    filterDrawerOpen: false,
   };
 
-  handleChange = (ev: any, selectedTab: any) => {
+  handleTabChange = (ev: any, selectedTab: any) => {
     if (selectedTab === 'nowPlaying') {
       this.props.onOpenFullPlayer();
       return;
     }
 
-    this.setState({ selectedTab });
+    if (selectedTab === 'filters') {
+      this.toggleFilterDrawer();
+      return;
+    }
 
-    switch (selectedTab) {
+    this.setState({ selectedTab });
+    this.navigateTo(selectedTab);
+  };
+
+  handleFilterChange = (filterId: string) => () => {
+    this.setState({ selectedTab: 'filters' });
+    this.toggleFilterDrawer();
+    this.navigateTo(filterId);
+  };
+
+  navigateTo = (page: string) => {
+    switch (page) {
       case 'subscriptions':
         this.props.history.push('/');
-        break;
-      case 'filters':
-        this.props.history.push('/playlist/recent');
         break;
       case 'discover':
         this.props.history.push('/search');
@@ -60,35 +95,62 @@ class BottomNav extends React.Component<BottomNavProps, BottomNavState> {
       case 'settings':
         this.props.history.push('/settings');
         break;
+      case 'filter-recent':
+        this.props.history.push('/playlist/recent');
+        break;
+      case 'filter-inProgress':
+        this.props.history.push('/playlist/inProgress');
+        break;
     }
+  };
+
+  toggleFilterDrawer = () => {
+    this.setState({ filterDrawerOpen: !this.state.filterDrawerOpen });
   };
 
   render() {
     const { classes, episode } = this.props;
 
     return (
-      <BottomNavigation
-        value={this.state.selectedTab}
-        className={classes.root}
-        onChange={this.handleChange}
-      >
-        <BottomNavigationAction label="Subs" icon={<AppsIcon />} value="subscriptions" />
-        <BottomNavigationAction label="Filters" icon={<ListIcon />} value="filters" />
-        <BottomNavigationAction
-          label="Play"
-          style={{ paddingTop: '2px' }}
-          icon={
-            episode ? (
-              <PlayerButton progress={75} episodeCover={episode.cover[100]} />
-            ) : (
-              <PlayIcon style={{ fontSize: '50px' }} />
-            )
-          }
-          value="nowPlaying"
-        />
-        <BottomNavigationAction label="Discover" icon={<SearchIcon />} value="discover" />
-        <BottomNavigationAction label="Settings" icon={<SettingsIcon />} value="settings" />
-      </BottomNavigation>
+      <React.Fragment>
+        <BottomNavigation
+          value={this.state.selectedTab}
+          className={classes.root}
+          onChange={this.handleTabChange}
+        >
+          <BottomNavigationAction label="Subs" icon={<AppsIcon />} value="subscriptions" />
+          <BottomNavigationAction label="Filters" icon={<ListIcon />} value="filters" />
+          <BottomNavigationAction
+            label="Play"
+            style={{ paddingTop: '2px' }}
+            icon={
+              episode ? (
+                <PlayerButton progress={75} episodeCover={episode.cover[100]} />
+              ) : (
+                <PlayIcon style={{ fontSize: '50px' }} />
+              )
+            }
+            value="nowPlaying"
+          />
+          <BottomNavigationAction label="Discover" icon={<SearchIcon />} value="discover" />
+          <BottomNavigationAction label="Settings" icon={<SettingsIcon />} value="settings" />
+        </BottomNavigation>
+        <Drawer
+          anchor="bottom"
+          open={this.state.filterDrawerOpen}
+          classes={{ paper: classes.filterDrawerPaper }}
+          onClose={this.toggleFilterDrawer}
+        >
+          <List>
+            {filters.map(filter => (
+              <ListItem button onClick={this.handleFilterChange(filter.id)} key={filter.id}>
+                <ListItemIcon>{filter.icon}</ListItemIcon>
+                <ListItemText>{filter.title}</ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+      </React.Fragment>
     );
   }
 }
