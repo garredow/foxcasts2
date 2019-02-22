ce19f7a48155160d58305e089dfb5553b25d7df2
Garrett Downs <choorp@gmail.com>
2019-02-21 20:36:06 -0500

    [EpisodeDetail] Refactor using hooks

--- src/components/EpisodeDetail.tsx
+++ src/components/EpisodeDetail.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import { withStyles, Theme, WithStyles } from '@material-ui/core/styles';
+import { Theme } from '@material-ui/core/styles';
 import formatTime from '../utils/formatTime';
 import { Episode } from '../models';
 import sanitizeHtml from 'sanitize-html';
+import { makeStyles } from '@material-ui/styles';
 
-const styles: any = (theme: Theme) => ({
+const useStyles = makeStyles((theme: Theme) => ({
   container: {
     textAlign: 'center',
     marginBottom: '20px',
@@ -24,49 +25,46 @@ const styles: any = (theme: Theme) => ({
       color: (theme.palette.secondary as any)[200],
     },
   },
-});
+}));
 
-type OwnProps = {
+type EpisodeDetailProps = {
   episode: Episode;
   onResume: (event: any) => void;
   onPlayFromBeginning: (event: any) => void;
   onTogglePlayed: (event: any) => void;
 };
 
-type EpisodeDetailProps = OwnProps & WithStyles;
+function EpisodeDetail(props: EpisodeDetailProps) {
+  const classes = useStyles();
+  const { episode } = props;
 
-class EpisodeDetail extends React.Component<EpisodeDetailProps, any> {
-  render() {
-    const { classes, episode } = this.props;
+  if (!episode) return null;
 
-    if (!episode) return null;
-
-    return (
-      <React.Fragment>
-        <div className={classes.container}>
-          <Typography variant="h6">{episode.title}</Typography>
-          <Typography variant="subtitle1">{episode.author}</Typography>
-        </div>
-        <div className={classes.actions}>
-          <Button variant="outlined" onClick={this.props.onPlayFromBeginning}>
-            {episode.progress > 0 ? 'Play From Beginning' : 'Play'}
+  return (
+    <React.Fragment>
+      <div className={classes.container}>
+        <Typography variant="h6">{episode.title}</Typography>
+        <Typography variant="subtitle1">{episode.author}</Typography>
+      </div>
+      <div className={classes.actions}>
+        <Button variant="outlined" onClick={props.onPlayFromBeginning}>
+          {episode.progress > 0 ? 'Play From Beginning' : 'Play'}
+        </Button>
+        {episode.progress > 0 && episode.progress < episode.duration && (
+          <Button variant="outlined" onClick={props.onResume}>
+            {`Resume at ${formatTime(episode.progress)}`}
           </Button>
-          {episode.progress > 0 && episode.progress < episode.duration && (
-            <Button variant="outlined" onClick={this.props.onResume}>
-              {`Resume at ${formatTime(episode.progress)}`}
-            </Button>
-          )}
-          <Button variant="outlined" onClick={this.props.onTogglePlayed}>
-            Mark as {episode.progress >= episode.duration ? 'Unplayed' : 'Played'}
-          </Button>
-        </div>
-        <div
-          className={classes.details}
-          dangerouslySetInnerHTML={{ __html: sanitizeHtml(episode.description) }}
-        />
-      </React.Fragment>
-    );
-  }
+        )}
+        <Button variant="outlined" onClick={props.onTogglePlayed}>
+          Mark as {episode.progress >= episode.duration ? 'Unplayed' : 'Played'}
+        </Button>
+      </div>
+      <div
+        className={classes.details}
+        dangerouslySetInnerHTML={{ __html: sanitizeHtml(episode.description) }}
+      />
+    </React.Fragment>
+  );
 }
 
-export default withStyles(styles, { withTheme: true })(EpisodeDetail);
+export default EpisodeDetail;
