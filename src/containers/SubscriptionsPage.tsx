83f675ea33610dfd1e13a813b627736b5c27569e
Garrett Downs <choorp@gmail.com>
2019-02-22 20:43:06 -0500

    [SubscriptionsPage] Refactor using hooks

--- src/containers/SubscriptionsPage.tsx
+++ src/containers/SubscriptionsPage.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import PodcastService from '../services/podcastService';
 import { Link } from 'react-router-dom';
 import AppContext from '../components/AppContext';
-import { withStyles, WithStyles, Theme } from '@material-ui/core/styles';
-import { Podcast, AppContextProps } from '../models';
+import { Theme } from '@material-ui/core/styles';
+import { Podcast } from '../models';
 import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 
 const podcastService = new PodcastService();
 
-const styles: any = (theme: Theme) => ({
+const useStyles = makeStyles((theme: Theme) => ({
   message: {
     padding: '30px 15px',
     textAlign: 'center',
@@ -26,61 +27,42 @@ const styles: any = (theme: Theme) => ({
     paddingBottom: '100%',
     backgroundSize: 'contain',
   },
-});
+}));
 
-type PodcastTileProps = {
-  classes: any;
-  podcast: Podcast;
-};
+function SubscriptionsPage() {
+  const [podcasts, setPodcasts] = useState<Podcast[]>([]);
+  const context = useContext(AppContext);
+  const classes = useStyles();
 
-const PodcastTile = ({ classes, podcast }: PodcastTileProps) => (
-  <Link to={{ pathname: `/podcast/${podcast.id}` }} rel="div">
-    <div className={classes.tile} style={{ backgroundImage: `url(${podcast.cover['600']}` }} />
-  </Link>
-);
-
-type SubscriptionsPageProps = WithStyles & AppContextProps;
-
-interface SubscriptionsPageState {
-  podcasts: Podcast[];
-}
-
-class SubscriptionsPage extends React.Component<SubscriptionsPageProps, SubscriptionsPageState> {
-  state: SubscriptionsPageState = { podcasts: [] };
-
-  componentDidMount() {
-    this.props.setAppTitle('Subscriptions');
+  useEffect(() => {
+    context.setAppTitle('Subscriptions');
 
     podcastService
       .getSubscriptions()
-      .then(podcasts => this.setState({ podcasts }))
+      .then(podcasts => setPodcasts(podcasts))
       .catch(err => console.error('Failed to get podcasts', err));
-  }
-  render() {
-    const { classes } = this.props;
+  }, []);
 
-    return (
-      <div className="page-container">
-        {this.state.podcasts.length === 0 && (
-          <Typography className={classes.message}>
-            It looks like you haven't subscribed to any podcasts yet.{' '}
-            <Link to="/search">Let's go find some!</Link>
-          </Typography>
-        )}
-        <div className={classes.gridList}>
-          {this.state.podcasts.map(podcast => (
-            <PodcastTile classes={classes} podcast={podcast} key={podcast.id} />
-          ))}
-        </div>
+  return (
+    <div className="page-container">
+      {podcasts.length === 0 && (
+        <Typography className={classes.message}>
+          It looks like you haven't subscribed to any podcasts yet.&nbsp;
+          <Link to="/search">Let's go find some!</Link>
+        </Typography>
+      )}
+      <div className={classes.gridList}>
+        {podcasts.map(podcast => (
+          <Link to={{ pathname: `/podcast/${podcast.id}` }} rel="div" key={podcast.id}>
+            <div
+              className={classes.tile}
+              style={{ backgroundImage: `url(${podcast.cover['600']}` }}
+            />
+          </Link>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-const ComponentWithContext = (props: any) => (
-  <AppContext.Consumer>
-    {({ setAppTitle }: any) => <SubscriptionsPage {...props} setAppTitle={setAppTitle} />}
-  </AppContext.Consumer>
-);
-
-export default withStyles(styles)(ComponentWithContext);
+export default SubscriptionsPage;
