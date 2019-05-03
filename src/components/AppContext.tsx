3a3f2f4c38a89a78334423b8efa5bb6d7d7c59fb
Garrett Downs <choorp@gmail.com>
2019-05-02 23:08:21 -0400

    Set AppContext type

--- src/components/AppContext.tsx
+++ src/components/AppContext.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import { AppContextProps } from '../models';
 
-const AppContext = React.createContext({});
+const AppContext = React.createContext<AppContextProps>({} as any);
 
 export default AppContext;
